acd55b608405408bf75604bb0986c17d531f46e3
Dominik Meglič <meglic@code4business.de>
2018-10-11 15:10:28 +0200

    Add fee to invoice and creditmemo.
    Signed-off-by: Dominik Meglič <meglic@code4business.de>

---
+++ Model/Total/Creditmemo/CashOnDeliveryFee.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types = 1);
+
+namespace Brandung\CashOnDeliveryFee\Model\Total\Creditmemo;
+
+class CashOnDeliveryFee extends \Magento\Sales\Model\Order\Creditmemo\Total\AbstractTotal
+{
+    /**
+     * @inheritdoc
+     */
+    public function collect(\Magento\Sales\Model\Order\Creditmemo $creditmemo)
+    {
+        parent::collect($creditmemo);
+
+        $codFee = $creditmemo->getOrder()->getExtensionAttributes()->getCashOnDeliveryFee();
+        $baseCodFee = $creditmemo->getOrder()->getExtensionAttributes()->getBaseCashOnDeliveryFee();
+
+        $creditmemo->setData(\Brandung\CashOnDeliveryFee\Model\Total\CashOnDeliveryFee::TOTAL_CODE, $codFee);
+        $creditmemo->setData(\Brandung\CashOnDeliveryFee\Model\Total\CashOnDeliveryFee::BASE_TOTAL_CODE, $baseCodFee);
+
+        if (round($codFee, 2) != 0)
+        {
+            $creditmemo->setGrandTotal($creditmemo->getGrandTotal() + $codFee);
+            $creditmemo->setBaseGrandTotal($creditmemo->getBaseGrandTotal() + $codFee);
+        }
+
+        return $this;
+    }
+}
